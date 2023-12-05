1fc080c65866344211c4387f90b8b838c7b9a555
Sea <94820710+sea-rod@users.noreply.github.com>
2023-12-05 18:58:35 +0530

    Backend (#7)
    * add to cart
    
    * list cart
    
    ---------
    
    Co-authored-by: sea <sea11rod@gmail.com>

--- xconnect_backend/canteen/views.py
+++ xconnect_backend/canteen/views.py
@@ -1,20 +1,41 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import Menu
-from .permissions import IsCanteenStaff
-from .serializer import MenuSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Menu,Cart
+from .permissions import IsCanteenStaff,IsUser
+from .serializer import MenuSerializer,ItemSerializer,CartSerializer
 
 
-class ListCreateMenu(ListCreateAPIView):
+class ListCreateMenu(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     permission_classes = (IsAuthenticated, IsCanteenStaff)
     parser_classes = (MultiPartParser, FormParser)
 
 
-class RetrieveUpdateDestroyMenu(RetrieveUpdateDestroyAPIView):
+class RetrieveUpdateDestroyMenu(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    permission_classes = (IsAuthenticated, IsCanteenStaff)
+    permission_classes = (IsAuthenticated,IsCanteenStaff)
     parser_classes = (MultiPartParser, FormParser)
+
+class CreateItem(generics.CreateAPIView):
+    serializer_class = ItemSerializer
+    permission_classes = (IsAuthenticated,)
+    
+    def perform_create(self, serializer):
+        cart_id = Cart.objects.get(user_id=self.request.user.id)
+        serializer.save(cart_id=cart_id)
+    
+class ListCart(generics.ListAPIView):
+    serializer_class = CartSerializer
+    permission_classes = (IsAuthenticated,IsUser)
+    queryset = Cart.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        cart = Cart.objects.get(user_id=request.user)
+        serializer = self.get_serializer(cart)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
