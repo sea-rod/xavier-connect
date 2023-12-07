343c7dc9d225f8e8605e7958309d1bec91a5a1ea
casey <casey346@gmail.com>
2023-12-07 18:48:24 +0530

    add navbar and footer

--- xconnect_frontend/xconnect/src/App.js
+++ xconnect_frontend/xconnect/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <Navbar/>
+    <h1>St. Xavier's College Mapusa, Goa</h1>
+    <h2>Connecting bla bla bla</h2>
+    <img src="st-xaviers-college.jpg" class="rounded mx-auto d-block" height={250} width={500} alt=""/><br />
+
+    <Footer/>
+    </>
   );
 }
 
