74e6d3686c9b96de39ae8d90590587b1ed5a2103
Sea <94820710+sea-rod@users.noreply.github.com>
2024-01-07 16:02:53 +0530

    Master to frontend  (#25)
    * Merge pull request #13 from sea-rod/backend (#14)
    
    * sub-event curd
    
    * event register done
    
    * registration emmail done
    
    * Merge pull request #21 from sea-rod/master (#23)
    
    * Merge pull request #21 from sea-rod/master (#24)
    
    ---------
    
    Co-authored-by: sea <sea11rod@gmail.com>

--- xconnect_backend/events/admin.py
+++ xconnect_backend/events/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Event,SubEvent
+from .models import Event, SubEvent, College, Participant
 
-admin.site.register(Event)
+
+class SubEventInline(admin.StackedInline):
+    model = SubEvent
+    extra = 1
+
+
+class SubEventAdmin(admin.ModelAdmin):
+    inlines = [SubEventInline]
+
+
+admin.site.register(Event, SubEventAdmin)
 admin.site.register(SubEvent)
+admin.site.register(College)
+admin.site.register(Participant)
