13f8dc1ebbbf71d2d2c7129c886ed345e7f93366
SelFerns <selwynfernandes17@gmail.com>
2024-01-09 20:44:39 +0530

    Change Dev Dependencies

--- xconnect_frontend/xconnect/src/components/loginForm.js
+++ xconnect_frontend/xconnect/src/components/loginForm.js
@@ -7,15 +7,15 @@ export default function LoginForm() {
             <div className="heading text-center">Sign In</div>
 
             <form action="" className="form form-floating">
-                <div class="form-floating mb-3">
+                <div className="form-floating mb-3">
                     <input type="email" class="form-control input" id="floatingInput" placeholder="" />
                     <label htmlFor="floatingInput" className="text-black-50">Email Address</label>
                 </div>
-                <div class="form-floating">
+                <div className="form-floating">
                     <input type="password" class="form-control input" id="floatingPassword" placeholder="" />
                     <label htmlFor="floatingPassword" className="text-black-50">Password</label>
                 </div>
-                <span className="forgot-password"><a href="#">Forgot Password?</a></span>
+                <span className="forgot-password"><a href="/">Forgot Password?</a></span>
                 <input className="btn btn-primary btn-block login-button" type="submit" value="Sign In" />
             </form>
         </div>
