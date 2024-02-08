47a20fcbe8ebe63fd30930c8ce8aec6072070539
sea <sea11rod@gmail.com>
2024-02-08 19:54:38 +0530

    added delete item

--- xconnect_backend/canteen/urls.py
+++ xconnect_backend/canteen/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ListCreateMenu, RetrieveUpdateDestroyMenu,CreateItem,ListCart,UpdateIten
+from .views import ListCreateMenu, RetrieveUpdateDestroyMenu,CreateItem,ListCart,UpdateDeleteItem
 
 urlpatterns = [
     path("menu/", ListCreateMenu.as_view()),
     path("<int:pk>/menu/", RetrieveUpdateDestroyMenu.as_view()),
     path("item/", CreateItem.as_view()),
-    path("<int:pk>/item/",UpdateIten.as_view()),
+    path("<int:pk>/item/",UpdateDeleteItem.as_view()),
     path("cart/",ListCart.as_view())
 ]
