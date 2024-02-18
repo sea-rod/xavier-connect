92a3c9e7b03f7e57d819a8149946de4e41a1b172
sea <sea11rod@gmail.com>
2024-02-18 21:02:38 +0530

    setup canteen api in forntend

--- xconnect_backend/canteen/models.py
+++ xconnect_backend/canteen/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class Menu(models.Model):
     item_name = models.CharField(max_length=255)
     price = models.FloatField()
-    image = models.ImageField(default="media/default.png", upload_to="media/canteen")
+    image = models.ImageField(default="media/default.jpg", upload_to="media/canteen")
     avail_quantity = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.item_name
 
+
 class Cart(models.Model):
-    user_id = models.OneToOneField(get_user_model(),on_delete=models.CASCADE)
+    user_id = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
-    total = models.DecimalField(max_digits=10,decimal_places=2,default=0)
-    menu_id = models.ManyToManyField(Menu,through="Items")
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    menu_id = models.ManyToManyField(Menu, through="Items")
+
 
 class Items(models.Model):
-    cart_id = models.ForeignKey(Cart,on_delete=models.CASCADE)
-    menu_id = models.ForeignKey(Menu,on_delete=models.CASCADE)
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
