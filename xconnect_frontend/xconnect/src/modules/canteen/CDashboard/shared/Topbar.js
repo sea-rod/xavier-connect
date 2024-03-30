bd144a21dcafe520edc22a9b534151435b575785
daniel barretto <dainelbarretto999@gmail.com>
2024-03-30 10:27:57 +0530

    fixed admin and navbar

--- xconnect_frontend/xconnect/src/modules/canteen/CDashboard/shared/Topbar.js
+++ xconnect_frontend/xconnect/src/modules/canteen/CDashboard/shared/Topbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Topbar = () => {
   return (
-    <div style={{display:"flex", justifyContent:"space-around", backgroundColor:"#FF7D34", width:"100vw", height:"auto"}}>
+    <div style={{display:"flex", justifyContent:"space-around", backgroundColor:"#FF7D34", height:"auto"}}>
             <div>
                 <h1 style={{ fontSize:"4vh", padding:"1vh" }}>XConnect</h1>
             </div>
