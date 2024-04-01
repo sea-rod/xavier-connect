5d9257e1dc60d46fe1c2bec7ecf48b34e4335917
SelFerns <selwynfernandes17@gmail.com>
2024-04-01 18:50:54 +0530

    Final Merge (Hero Section)

--- xconnect_frontend/xconnect/src/pages/Home/components/HeroSection.js
+++ xconnect_frontend/xconnect/src/pages/Home/components/HeroSection.js
@@ -6,19 +6,11 @@ function HeroSection() {
 
   return (
     <>
-<<<<<<< HEAD
      <div className='container vh-100'>
      <div class="px-4 text-center border-bottom">
     <h1 class="display-4 fw-bold text-body-emphasis mt-3">Welcome to XConnect: Shaping Futures, Igniting Minds</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">At XConnect, we believe in the transformative power of education. Step into a world where academic excellence meets vibrant campus life, and where passion for learning fuels endless possibilities. As a beacon of knowledge and innovation, we are dedicated to nurturing the leaders and thinkers of tomorrow.</p>
-=======
-     <div className='container'>
-     <div className="px-4 text-center border-bottom">
-    <h1 className="display-4 fw-bold text-body-emphasis">Welcome to XConnect: Shaping Futures, Igniting Minds</h1>
-    <div className="col-lg-6 mx-auto">
-      <p className="lead mb-4">At XConnect, we believe in the transformative power of education. Step into a world where academic excellence meets vibrant campus life, and where passion for learning fuels endless possibilities. As a beacon of knowledge and innovation, we are dedicated to nurturing the leaders and thinkers of tomorrow.</p>
->>>>>>> 79713361347e9b152404be02c8a42ff21a7a017d
     </div>
     <div>
       <div className="container">
