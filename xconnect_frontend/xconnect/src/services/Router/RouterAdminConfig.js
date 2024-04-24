53745cf18c460fef09978c7484bd6274713566f9
SelFerns <selwynfernandes17@gmail.com>
2024-04-24 14:19:56 +0530

    pulling new changes

--- xconnect_frontend/xconnect/src/services/Router/RouterAdminConfig.js
+++ xconnect_frontend/xconnect/src/services/Router/RouterAdminConfig.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Cdash from "../../modules/canteen/CDashboard/Cdash";
+import Cdash from "../../modules/canteen/CDashboard/cdash";
 
 const AdminRoutes = () => {
   return (
