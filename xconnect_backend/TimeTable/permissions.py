44bc73e55de76cbc20cb796a0e95303be4208fc3
sea <sea11rod@gmail.com>
2024-04-27 03:36:34 +0530

    tt fixed

--- xconnect_backend/TimeTable/permissions.py
+++ xconnect_backend/TimeTable/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework import permissions
 from django.contrib.auth.models import Group
 
+
 class IsTeacherOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return  request.user.group == Group.objects.get(name="teacher")
+        return (
+            request.user.group == Group.objects.get(name="teacher")
+            or request.user.is_superuser
+        )
