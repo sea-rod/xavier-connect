3497277410177ceeb7d8b78b90cbfc48e02bcde9
Immortalsoul225 <danielbarretto999@gmail.com>
2024-04-26 20:23:03 +0530

    added intergrated calendar files

---
+++ xconnect_frontend/xconnect/src/modules/calendar/Calendar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Calendar.css"
+import { useState, useEffect } from "react";
+import axiosInstance from "../../services/axios";
+
+export default function Calendar() {
+
+    const [announcementData, setAnnouncementData] = useState([]);
+
+    const fetchData = () => {
+        axiosInstance.get("calendar/event/").then((res) => {
+          console.log(res.data);
+          setAnnouncementData(res.data.map((item) => ({
+            ...item,
+            dayOfWeek: getDayOfWeek(item.date),
+            dayOfMonth: item.date.split("-")[2] 
+          })));
+        });
+      };
+    
+      useEffect(() => {
+        fetchData();
+      }, []);
+      
+      const getDayOfWeek = (dateString) => {
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        const date = new Date(dateString);
+        return days[date.getDay()];
+      };
+
+
+    return (
+        <>
+        <div className="container">
+            <div className='mt-3 d-flex justify-content-center align-items-center calendar-hero-font' style={{backgroundImage: `url(${process.env.PUBLIC_URL}/images/Calendar/image33.png)`, height:'200px'}}>March 2021</div>
+            
+           
+            {announcementData.map((data,index) => (
+                <div key = {index}>
+                    <div style={{display: "inline", color:'#55C1A3'}}>{data.dayOfWeek} <span style={{fontSize:'25px'}}><strong>{data.dayOfMonth}</strong></span></div>
+
+                    <div className="mt-3 announcement-font" style={{backgroundColor: '#5CC1A3', height: 'auto', padding: "10px", borderRadius:"12px", display: "inline-block", width: "80%", marginLeft: "10px"}}>{data.name}</div>
+                </div>
+            ))}
+                 
+        </div>
+        </>
+    );
+}
