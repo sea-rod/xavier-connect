83e546f1e6a4be22ba3b7f8bc5d971572c070f02
SelFerns <selwynfernandes17@gmail.com>
2024-04-27 01:00:33 +0530

    getting pagehead back

--- xconnect_frontend/xconnect/src/services/Router/RouterAdminConfig.js
+++ xconnect_frontend/xconnect/src/services/Router/RouterAdminConfig.js
@@ -1,8 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import Cdash from "../../modules/canteen/CDashboard/cdash";
+import axiosInstance from "../axios";
+import Cdash from "../../modules/canteen/CDashboard/CDash";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import EventsDash from "../../modules/events/Admin/eventsDashBoard";
 
 const AdminRoutes = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("access_token")) {
+      navigate("/login");
+    }
+    axiosInstance.get("accounts/user-group/").then((res) => {
+      if (
+        !(
+          res.data.is_superuser ||
+          res.data.group == "canteen_staff" ||
+          res.data.group == "library_staff" ||
+          res.data.group == "teacher"
+        )
+      ) {
+        navigate("/login");
+      }
+    });
+  }, []);
   return (
     <Routes>
       <Route path="canteen/*" element={<Cdash />} />
